aa5aaf3c3227c703c895ad368e7664732c901797
Bartzlo <ksb.bart@gmail.com>
2017-10-13 15:39:39 +0500

    Create starting template

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,2 @@
 'use strict';
-console.log('start main');
-
-function fun() {
-  const Animal = require('./Animal.js');
-
-  let animal = new Animal(33);
-  animal.getFootsCount();
-  animal.cutFoot();
-  animal.cutFoot();
-  animal.cutFoot();
-  animal.cutFoot();
-  animal.cutFoot();
-  animal.getFootsCount();
-}
-
-setTimeout(fun, 5000);
+console.log('start main script');
