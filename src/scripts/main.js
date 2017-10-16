8ac539086e0c271312b0731aac493da59e5b4808
Bartzlo <ksb.bart@gmail.com>
2017-10-16 18:18:46 +0500

    Add Mustache

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,16 @@
 'use strict';
 console.log('start main script');
+//********************************//
+
+const Mustache = require('mustache');
+
+var view = {
+  title: "Joe",
+  calc: function () {
+    return 2 + 4;
+  }
+};
+
+var output = Mustache.render("{{title}} spends {{calc}}", view);
+
+document.body.append(output);
